b2a1bc72e9ba2ac5db95cbfc73a5f6edc77c0f13
dswonger <138087816+dswonger@users.noreply.github.com>
2023-10-11 12:04:31 -0400

    Delete java-blue-minicapstonemodule1-team8 directory

--- java-blue-minicapstonemodule1-team8/src/main/java/com/techelevator/Gum.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator;
-
-import java.math.BigDecimal;
-
-public class Gum extends Item {
-    public Gum(BigDecimal price, String slotLocation, String productName, int stock, String dispenseMessage, int fullPriceSaleCounter, int discountSaleCounter) {
-        super(price, slotLocation, productName, stock, dispenseMessage, fullPriceSaleCounter, discountSaleCounter);
-    }
-}
